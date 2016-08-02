f1d1933e972c4169b18c8eb424ccaaf0b0642235
leglars.Zheng <leglars@gmail.com>
2016-08-03 01:09:02 +1000

    init hero

--- build/js/hero.js
+++ build/js/hero.js
@@ -1,3 +1,35 @@
 /**
  * Created by llegl on 2016/8/3.
  */
+
+$(document).ready(function() {
+    setup()
+});
+
+$(window).resize(function() {
+    redimensionHero();
+});
+
+
+function setup() {
+    startFullSizeHero()
+}
+
+function startFullSizeHero() {
+    if ($('#full-screen-slider').length) {
+        var windowHeight = $(window).height();
+        console.log(windowHeight);
+        // var windowWidth = $(window).width();
+        $('#full-screen-slider').css({
+            'height': windowHeight,
+            'width': "100%"
+        });
+
+        $('#full-screen-slider .hero-container > img').css({
+            'height': windowHeight,
+            'width': "100%"
+        })
+    }
+}
+
+function redimensionHero() {}
