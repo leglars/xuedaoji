37459f5db7cb7755110cefc2bee63fd482711924
leglars.Zheng <leglars@gmail.com>
2016-08-14 01:51:40 +1000

    add our team section and footer

--- build/js/hero.js
+++ build/js/hero.js
@@ -7,18 +7,27 @@ $(document).ready(function() {
 });
 
 $(window).resize(function() {
-    resizeElement.setFullSizeHero()
-
+    if ($(window).width() > 768) {
+        resizeElement.setAboutUsFullSize();
+        resizeElement.setOurTeamFullSize();
+    } else {
+        resizeElement.resetOtherSectionsHeight();
+    }
+    resizeElement.setFullSizeHero();
 });
 
 
 function setup() {
-    resizeElement.setFullSizeHero()
+    if ($(window).width() > 768) {
+        resizeElement.setAboutUsFullSize();
+        resizeElement.setOurTeamFullSize();
+    }
+    resizeElement.setFullSizeHero();
 }
 
 var resizeElement = {
     setFullSizeHero: function () {
-        var fullScreenSlider = $('#full-screen-slider')
+        var fullScreenSlider = $("#full-screen-slider");
         if (fullScreenSlider.length) {
             var windowHeight = $(window).height();
             console.log(windowHeight);
@@ -39,4 +48,47 @@ var resizeElement = {
             // })
         }
     },
+
+    setAboutUsFullSize: function() {
+        var aboutUs = $("#aboutUs");
+        var windowHeight = $(window).height();
+        if (windowHeight > 700) {
+            aboutUs.css({
+                'height': windowHeight,
+                'width': "100%"
+            });
+        }else {
+            aboutUs.css({
+                'height': 700,
+                'width': "100%"
+            });
+        }
+    },
+
+    setOurTeamFullSize: function() {
+        var ourTeam = $("#ourTeam");
+        var windowHeight = $(window).height();
+        if (windowHeight > 700) {
+            ourTeam.css({
+                'height': windowHeight,
+                'width': "100%"
+            });
+        }else {
+            ourTeam.css({
+                'height': 700,
+                'width': "100%"
+            });
+        }
+    },
+
+    resetOtherSectionsHeight: function() {
+        var aboutUs = $("#aboutUs");
+        var ourTeam = $("#ourTeam");
+        aboutUs.css({
+            'height': 'auto'
+        });
+        ourTeam.css({
+            'height': 'auto'
+        })
+    }
 };
